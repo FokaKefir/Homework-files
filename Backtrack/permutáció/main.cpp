6d4f7fce4e7506a47eb06fe723ed80ed4a899686
FokaKefir <babosdavid8@gmail.com>
2020-01-29 16:11:30 +0200

    Adding the backtrack folder and renaming the pointerek folder

---
+++ Backtrack/permutáció/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+#define N 16
+#define DUPLA 1
+#define NEM_DUPLA 0
+
+using namespace std;
+
+void kiVektor(int n, int v[]){
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+}
+
+bool dupla(int n, int v[], int k){
+    int s = 0;
+    for (int i = 0; i < n; ++i) {
+        if(v[i] == k)
+            s++;
+    }
+    if(s==1)
+        return NEM_DUPLA;
+    else
+        return DUPLA;
+
+}
+
+void backtracking(int n){
+    int v[N] = {0};
+    int index = 0;
+    v[index] = 1;
+    while(index >= 0){
+        if(v[index] > n){
+            v[index] = 0;
+            index--;
+            v[index]++;
+        }
+
+        if(dupla(n, v, v[index]) == DUPLA){
+            v[index]++;
+        } else if(v[index] <= n){
+            if(index < n-1){
+                v[++index] = 1;
+            }else{
+                kiVektor(n, v);
+                v[index]++;
+            }
+        }
+    }
+}
+
+int main(){
+    int n = 5;
+    backtracking(n);
+}
