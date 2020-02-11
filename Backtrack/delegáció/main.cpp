edb3373e3f08ba46d22adce891f11651ace7c198
FokaKefir <babosdavid8@gmail.com>
2020-02-11 17:26:43 +0200

    Adding new folder: delegácó.

---
+++ Backtrack/delegáció/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+#define N 15
+#define DUPLA 1
+#define NEM_DUPLA 0
+
+using namespace std;
+
+void kiVektor(int n, int v[]){
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+}
+
+bool dupla(int n, int v[], int k){
+    int s = 0;
+    for (int i = 0; i < n; ++i) {
+        if(v[i] == k)
+            s++;
+    }
+    if(s==1)
+        return NEM_DUPLA;
+    else
+        return DUPLA;
+
+}
+
+void backtracking(int n, int p, int f, int r){
+    int v[N]={0};
+    int index = 0;
+    v[index] = 1;
+    while(index>=0){
+        if((v[index] > (n-p+index+1)) or (index < r and v[index] > f)){
+            v[index] = 0;
+            index--;
+            v[index]++;
+        }else {
+            if (dupla(p, v, v[index]) == DUPLA) {
+                v[index]++;
+            }else if(index >= r and v[index] <= f){
+                v[index]++;
+            } else {
+                if(index < p-1){
+                    index++;
+                    v[index] = v[index - 1] + 1;
+                }else{
+                    kiVektor(p, v);
+                    v[index]++;
+                }
+
+            }
+        }
+    }
+}
+
+int main(){
+    freopen("file.in", "r", stdin);
+    int n = 8, p = 6, f = 4, r = 3;
+    backtracking(n, p, f, r);
+
+}
